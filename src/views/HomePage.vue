ee5576c7c4ec91a3ea204dcfbfc377bc2acef498
Tomek Popek <55105998+hekx11@users.noreply.github.com>
2022-05-15 17:14:43 +0200

    Login Page
    File cleaning, login page, theme setup

---
+++ src/views/HomePage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home"></div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+@Component
+export default class HomePage extends Vue {}
+</script>
